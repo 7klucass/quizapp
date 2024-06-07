aa26db91822b47df0740052b2ab2e937f241af0d
lucas soares <dossantos.lucas@eaportal.org>
2024-06-07 02:51:39 +0000

    Adicionada a função de jogar novamente e finalização do projeto

--- pages/resultado/resultado.js
+++ pages/resultado/resultado.js
@@ -3,12 +3,17 @@ import { trocarTema, verificarTema } from "../../helpers/tema-helper.js"
 const botaoTema = document.querySelector(".tema button")
 const body = document.querySelector("body")
 const assunto = localStorage.getItem("assunto")
+const botaoJogarNovamente = document.querySelector("main button")
 
 botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema)
 })
 
+botaoJogarNovamente.addEventListener("click", jogarNovamente)
+
 verificarTema(body, botaoTema)
+
+
 
 function alterarAssunto() {
     const divIcone = document.querySelector(".assunto_icone")
@@ -34,4 +39,13 @@ function inserirResultado(){
         <strong>${pontos}</strong>
 
         <p>de 10</p>`
+
 }
+
+function jogarNovamente(){
+    localStorage.removeItem("pontos")
+    localStorage.removeItem("assuntos")
+
+    window.location.href = "../../index.html" 
+}
+ inserirResultado()
