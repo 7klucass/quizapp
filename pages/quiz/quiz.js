cf31276e66209811b0655bc071e96f9dfd0d9040
lucas soares <dossantos.lucas@eaportal.org>
2024-06-08 01:41:14 +0000

    CORREÇÂO DO ERRO

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -33,7 +33,7 @@ function alterarAssunto() {
 
 async function buscarPerguntas(){
     const urlDados = "../../data.json"
-    await fetch(url).then(resposta => resposta.json()).then(dados => {
+    await fetch(urlDados).then(resposta => resposta.json()).then(dados => {
         dados.quizzes.forEach(dado => {
             if(dado.title === assunto){
                 quiz = dado
@@ -45,65 +45,59 @@ async function buscarPerguntas(){
 function montarPergunta() {
     const main = document.querySelector("main")
 
-    main.innerHTML = `<main>
-        <section class="pergunta">
+    main.innerHTML = `
+    <section class="pergunta">
             <div>
                 <p>Questão ${pergunta} de 10</p>
 
-                <h2>${alterarSinais(quiz.questions[pergunta-1].question)}</h2>
+                <h2>${alterarSinais(quiz.questions[pergunta - 1].question)}</h2>
             </div>
             <div class="barra_progresso">
-                <div style="width: ${pergunta * 10}%"></div>
+                <div style="width: ${ pergunta * 10 }%"></div>
             </div>
         </section>
 
         <section class="alternativas">
             <form action="">
                 <label for="alternativa_a">
-                    <input type="radio" id="alternativa_a" name="alternativa" value= "${alterarSinais(quiz.questions[pergunta-1].options[0])}">
+                    <input type="radio" id="alternativa_a" name="alternativa" value="${alterarSinais(quiz.questions[pergunta - 1].options[0])}">
 
                     <div>
                         <span>A</span>
-                        ${alterarSinais(quiz.questions[pergunta-1].options[0])}
-                        Hyper Trainer Marking Language
+                        ${alterarSinais(quiz.questions[pergunta - 1].options[0])}
                     </div>
                 </label>
-
                 <label for="alternativa_b">
-                    <input type="radio" id="alternativa_b" name="alternativa" value"${alterarSinais(quiz.questions[pergunta-1].options[1])}">
+                    <input type="radio" id="alternativa_b" name="alternativa" value="${alterarSinais(quiz.questions[pergunta - 1].options[1])}">
 
                     <div>
                         <span>B</span>
-                        ${alterarSinais(quiz.questions[pergunta-1].options[1])}
-                        Hyper Text Marketing Language
+                        ${alterarSinais(quiz.questions[pergunta - 1].options[1])}
                     </div>
                 </label>
-
                 <label for="alternativa_c">
-                    <input type="radio" id="alternativa_c" name="alternativa" value= "${alterarSinais(quiz.questions[pergunta-1].options[1])}">
+                    <input type="radio" id="alternativa_c" name="alternativa" value="${alterarSinais(quiz.questions[pergunta - 1].options[2])}">
 
                     <div>
                         <span>C</span>
-                        ${alterarSinais(quiz.questions[pergunta-1].options[1])}
-                        Hyper Text Markup Language
+                        ${alterarSinais(quiz.questions[pergunta - 1].options[2])}
                     </div>
                 </label>
-
                 <label for="alternativa_d">
-                    <input type="radio" id="alternativa_d" name="alternativa" value="${alterarSinais(quiz.questions[pergunta-1].options[3])}">
+                    <input type="radio" id="alternativa_d" name="alternativa" value="${alterarSinais(quiz.questions[pergunta - 1].options[3])}">
 
                     <div>
                         <span>D</span>
-                        ${alterarSinais(quiz.questions[pergunta-1].options[3])}
-                        Hyper Text Markup Leveler
+                        ${alterarSinais(quiz.questions[pergunta - 1].options[3])}
                     </div>
                 </label>
             </form>
 
             <button>Responder</button>
         </section>
-    </main>`
+    `
 }
+
 
 function alterarSinais(texto){
     return texto.replace(/</g, "&lt;").replace(/>/g, "&gt;")
@@ -112,6 +106,7 @@ function alterarSinais(texto){
 function guardarResposta(evento){
     resposta = evento.target.value
     idInputResposta = evento.target.id
+
     const botaoEnviar = document.querySelector(".alternativas button")
     botaoEnviar.addEventListener("click", validarResposta)
 }
@@ -126,14 +121,14 @@ function validarResposta(){
         botaoEnviar.innerText = "Finalizar"
         botaoEnviar.addEventListener("click", finalizar)
     } else{
-        botaoEnviar.removeEventListener("click", proximaPergunta)
+        botaoEnviar.addEventListener("click", proximaPergunta)
     }
     if (resposta === quiz.questions[pergunta-1].answer) {
         document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "correta")
         pontos = pontos + 1
     } else{
         document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "errada")
-        document.querySelector(`label[for='${respostaCorretaId}']`).setAttribute("id", "correta")
+        document.querySelector(`label[for='${respostaCorretaId}']`).setAttribute("id","correta")
     }
     pergunta = pergunta +1
 }
@@ -154,7 +149,7 @@ function adicionarEventoInputs(){
     inputsResposta.forEach(input =>{
         input.addEventListener("click", guardarResposta)
 
-        if(input.value === quiz.questions [pergunta-1].answer){
+        if(input.value === quiz.questions[pergunta-1].answer){
             respostaCorretaId = input.id
         }
     })
@@ -164,7 +159,7 @@ async function iniciar(){
     alterarAssunto()
     await buscarPerguntas()
     montarPergunta()
-
+    adicionarEventoInputs()
 }
 
 iniciar()
--- script.js
+++ script.js
@@ -16,7 +16,6 @@ botoesAssunto.forEach(botao => {
     function selecionarAssunto(evento){
         const classeBotao = evento.target.className
         const assunto = document.querySelector(`.${classeBotao} span`).innerText
-        const assunto = evento.target.innerText
         localStorage.setItem("assunto", assunto)
         window.location.href = "./pages/quiz/quiz.html"
     }
